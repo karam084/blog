4d6a9febbf04801bd4e5bad123a0dd089250dd50
karam084 <karam.ezz84@gmail.com>
2024-05-11 16:12:17 +0300

    Update users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.create(name: "Ahmed")
+user2 = User.create(name: "Karam")
+
+post = Post.create(title: "Ruby on Rails", author: user, text: "Ruby on Rails is a web application framework written in Ruby under the MIT License. Rails is a model–view–controller (MVC) framework, providing default structures for a database, a web service, and web pages.")
+post2 = Post.create(title: "Ruby on Rails", author: user2, text: "Ruby on Rails is a web application framework written in Ruby under the MIT License. Rails is a model–view–controller (MVC) framework, providing default structures for a database, a web service, and web pages.")
+
+comment = Comment.create(text: "Great post!", author: user, post: post)
+comment2 = Comment.create(text: "Great post!", author: user2, post: post2)
+
+like = Like.create(author: user, post: post)
+like2 = Like.create(author: user2, post: post2)
